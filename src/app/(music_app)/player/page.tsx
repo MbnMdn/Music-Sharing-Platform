9aaa2598a40223bd55a77fa5427de1563d133c17
mbina <mbn.mdn.82@gmail.com>
2024-07-12 21:30:50 +0330

    small issue with player fixed

--- src/app/(music_app)/player/page.tsx
+++ src/app/(music_app)/player/page.tsx
@@ -2,7 +2,7 @@ import GlassPlayer from "@/app/ui/dashboard/glass-player";
 
 export default function Page() {
     return (
-        <div className="flex items-center justify-center fixed inset-0">
+        <div className="mobile-only flex items-center justify-center fixed inset-0">
             <div className="w-64 mt-20">
                 <GlassPlayer shadow={false}/>
             </div>
