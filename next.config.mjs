7c3205f70f8a1d79c71ddd04dbf550d8667875fe
mbina <mbn.mdn.82@gmail.com>
2024-07-16 01:28:59 +0330

    fixing auth callback

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'websong.liara.run',
+                hostname: 'websong-api.liara.run',
                 port: '',
                 pathname: '/**',
             },
